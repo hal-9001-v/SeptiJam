27e41e87ef80a9df95928a379dfbf1ebbdba5f31
dexaxi <dexaxi12@gmail.com>
2022-09-21 20:20:56 +0200

    Car changes

--- Assets/Scripts/Player/Car.cs
+++ Assets/Scripts/Player/Car.cs
@@ -3,10 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.Rendering;
+
+public enum WheelSize
+{
+    SMALL = 1,
+    MEDIUM = 2,
+    BIG = 3
+}
+
+
 
 [RequireComponent(typeof(Rigidbody))]
 public class Car : MonoBehaviour
 {
+
+    [HideInInspector]
+    public const float DEFAULT_WHEEL_SIZE = 0.35f;
+    public const float DEFAULT_WHEEL_SIZE_MODIFIER = 0.1f;
+    public const float DEFAULT_SQUARE_WHEEL_CV_MODIFIER = 1.65f;
+    
+    [Header("Car Modifiers")]
+    public CarModifierInfo carModifierInfo;
 
     [Header("Spawn")]
     [SerializeField] Vector3 spawnOffset;
@@ -29,9 +47,7 @@ public class Car : MonoBehaviour
     [Range(0, 1)]
     public float KeepGrip = 1f;
     public float Grip = 5f;
-
-    [HideInInspector]
-    public CarModifierInfo carModifierInfo;
+    
 
     [HideInInspector]
     public InputStr Input;
@@ -45,13 +61,15 @@ public class Car : MonoBehaviour
     {
         tracker.spawnCallback += Spawn;
         Rigidbody = GetComponent<Rigidbody>();
-        Rigidbody.centerOfMass = CenterOfMass;
-        SetCarModifierInfoDefaultStats();
+        Rigidbody.centerOfMass = CenterOfMass; 
+        
+        // SetCarModifierInfoDefaultStats();
+        OnChangeCarPiece();
         OnValidate();
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
         for (int i = 0; i < Wheels.Length; i++)
         {
@@ -61,7 +79,7 @@ public class Car : MonoBehaviour
                 Wheels[i].WheelCollider.steerAngle = Input.Steer * SteerAngle;
 
             Wheels[i].Rotation += Wheels[i].WheelCollider.rpm / 60 * 360 * Time.fixedDeltaTime;
-            Wheels[i].MeshRenderer.localRotation = Wheels[i].MeshRenderer.parent.localRotation * Quaternion.Euler(Wheels[i].Rotation, -Wheels[i].WheelCollider.steerAngle, 0);
+            Wheels[i].MeshRenderer.localRotation = Wheels[i].MeshRenderer.parent.localRotation * Quaternion.Euler(Wheels[i].Rotation, Wheels[i].WheelCollider.steerAngle, 0);
 
         }
 
@@ -83,28 +101,49 @@ public class Car : MonoBehaviour
     //TEMP QUICK AND DIRTY
     void SetCarModifierInfoDefaultStats()
     {
+        carModifierInfo.carMass = 2800f;
+        carModifierInfo.motorForce = 5000f;
+        carModifierInfo.steerAngle = 35f;
+        carModifierInfo.centerOfMass = new Vector3(0f, -2f, 0.159f);
+
         carModifierInfo.springForce = 35000f;
         carModifierInfo.damp = 4500f;
         carModifierInfo.springLength = 1f;
-        carModifierInfo.wheelRadius = 0.3f;
+        carModifierInfo.wheelSize = WheelSize.MEDIUM;
+        carModifierInfo.squareWheels = true;
 
-        carModifierInfo.motorForce = 5000f;
-        carModifierInfo.steerAngle = 35f;
-
-        carModifierInfo.carMass = 2800f;
-        carModifierInfo.centerOfMass = new Vector3(0f, -2f, 0.159f);
-
-        OnChangeCarPiece();
     }
 
     //Call this when modifying any car attribs
     void OnChangeCarPiece()
     {
+        
+        //Car stats
         Rigidbody.mass = carModifierInfo.carMass;
-        MotorPower = carModifierInfo.motorForce;
         SteerAngle = carModifierInfo.steerAngle;
         CenterOfMass = carModifierInfo.centerOfMass;
-        ModifyWheelStats(carModifierInfo.springForce, carModifierInfo.damp, carModifierInfo.springLength, carModifierInfo.wheelRadius);
+
+        if (carModifierInfo.squareWheels)
+            MotorPower = carModifierInfo.motorForce * DEFAULT_SQUARE_WHEEL_CV_MODIFIER;
+        else
+            MotorPower = carModifierInfo.motorForce;
+       
+        //wheel stats
+        float wheelRadius;
+        switch (carModifierInfo.wheelSize)
+        {
+            case WheelSize.SMALL:
+                 wheelRadius = DEFAULT_WHEEL_SIZE - DEFAULT_WHEEL_SIZE_MODIFIER;
+                break;
+            case WheelSize.BIG:
+                wheelRadius = DEFAULT_WHEEL_SIZE + DEFAULT_WHEEL_SIZE_MODIFIER;
+                break;
+            case WheelSize.MEDIUM:
+            default:
+                wheelRadius = DEFAULT_WHEEL_SIZE;
+                break;
+        }
+        ModifyWheelStats(carModifierInfo.springForce, carModifierInfo.damp, carModifierInfo.springLength, wheelRadius,carModifierInfo.squareWheels);
     }
 
     void OnValidate()
@@ -128,10 +167,10 @@ public class Car : MonoBehaviour
             Wheels[i].WheelCollider.forwardFriction = ffriction;
             Wheels[i].WheelCollider.sidewaysFriction = sfriction;
         }
-
+        
     }
 
-    private void ModifyWheelStats(float spring, float damp, float springLength, float wheelRadius)
+    private void ModifyWheelStats(float spring, float damp, float springLength, float wheelRadius, bool squareWheels)
     {
         for (int i = 0; i < Wheels.Length; i++)
         {
@@ -144,6 +183,18 @@ public class Car : MonoBehaviour
             Wheels[i].WheelCollider.suspensionSpring = wheelColliderSuspensionSpring;
             Wheels[i].WheelCollider.radius = wheelRadius;
             Wheels[i].WheelCollider.suspensionDistance = springLength;
+
+            Wheels[i].WheelCollider.GetComponentInChildren<BoxCollider>().enabled = squareWheels;
+            
+            if (squareWheels)
+            {
+                
+                float wheelSizeModifier = 1 - DEFAULT_WHEEL_SIZE  + wheelRadius; 
+                Debug.Log(wheelSizeModifier + " " + wheelRadius);
+                Wheels[i].WheelCollider.GetComponentInChildren<BoxCollider>().size *= wheelSizeModifier;
+                          
+            }
+
         }
     }
 
@@ -165,17 +216,31 @@ public class Car : MonoBehaviour
         public float Rotation;
     }
 
+    [System.Serializable]
     public struct CarModifierInfo
     {
+        
+        [Header("Car Stats")]
         public float carMass; //base mass 2.800
-        public float springForce; //base force 35.000
-        public float damp; //base damp 4.500
-        public float springLength; //base length 1
-        public float wheelRadius; // base radius 0.3
         public float motorForce; // base force 5000
         public float steerAngle; // base angle 35
         public Vector3 centerOfMass; // base COM 0, -2, 0.159  WARNING: Tofu car TODO: CHANGE
+        
+        [Header("Wheel Stats")]
+        public float springForce; //base force 35.000
+        public float damp; //base damp 4.500
+        public float springLength; //base length 1
+        public WheelSize wheelSize;
+        public bool squareWheels;
 
+
+    }
+
+
+    [ContextMenu("UpdateCar")]
+    public void UdpateCarDebug()
+    {
+        OnChangeCarPiece();
     }
 
 }
