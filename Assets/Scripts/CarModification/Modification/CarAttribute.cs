918c2469ed8ce14aa4c76af66a2840acc20a69ed
Celtia <celtiagamedev@gmail.com>
2022-09-24 14:33:03 +0200

    Debug Accesory added

--- Assets/Scripts/CarModification/Modification/CarAttribute.cs
+++ Assets/Scripts/CarModification/Modification/CarAttribute.cs
@@ -11,7 +11,7 @@ public class CarAttribute
     private static Dictionary<CarVarsType, float> CAR_VAR_MULTIPLIERS = new Dictionary<CarVarsType, float>() {
         { CarVarsType.MotorForce, 750 },
         { CarVarsType.SteerAngle, 10 },
-        { CarVarsType.Weight, 500 },
+        { CarVarsType.Weight, 250 },
         {CarVarsType.Turbo,500 }
     };
 
