f8c08e54ac59b6ac22a908ff33c378c953f29207
dexaxi <dexaxi12@gmail.com>
2022-09-26 17:24:05 +0200

    AAAAAAAAAAAAAA

--- Assets/Scripts/Menus/Popup.cs
+++ Assets/Scripts/Menus/Popup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 public enum CarParts
@@ -68,10 +69,8 @@ public class Popup : MonoBehaviour
         }
 
         popupImage.gameObject.SetActive(true);
-        CarParts carpart;
-        
 
-        
+
     }
 
     public void DisablePopup()
@@ -81,194 +80,14 @@ public class Popup : MonoBehaviour
     
     public void ChangePopup(CarParts car)
     {
-        switch (car){
-                case CarParts.anchor:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.barrel:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.beachball:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.bowlingball:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.butane:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.coconut:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.colacao:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.fan:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.flemish:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.floater:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.hamsterball:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.hooka:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.jagger:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.laptop:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.madrid:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.mercadona:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.microwave:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.monster:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.nokia:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.oliveoil:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.paellera:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.playcontrol:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.potato:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.rtx:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.rubberchicken:
-                    popupImage.sprite = images[0];
-                    break;
-                case CarParts.rudder:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.shovel:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.surftable:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.tofu:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.wine:
-                    popupImage.sprite = images[0];
-                break;
-        }
+        Debug.Log(car);
+        popupImage.sprite = images[(int)car+1];
+
     }
     
      public void ChangePopupEng(CarParts car)
     {
-        switch (car){
-                case CarParts.anchor:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.barrel:
-                    popupImage.sprite = images[0];
-                break;
-                case CarParts.beachball:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.bowlingball:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.butane:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.coconut:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.colacao:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.fan:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.flemish:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.floater:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.hamsterball:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.hooka:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.jagger:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.laptop:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.madrid:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.mercadona:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.microwave:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.monster:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.nokia:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.oliveoil:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.paellera:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.playcontrol:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.potato:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.rtx:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.rubberchicken:
-                    popupImage.sprite = engLishimages[0];
-                    break;
-                case CarParts.rudder:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.shovel:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.surftable:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.tofu:
-                    popupImage.sprite = engLishimages[0];
-                break;
-                case CarParts.wine:
-                    popupImage.sprite = engLishimages[0];
-                break;
-        }
+        popupImage.sprite = engLishimages[(int)car+1];
     }
     
     
